15b5204ec1639452f80c48ec9d31ca71c75e1e86
Slak44 <sylviu44@gmail.com>
2015-11-01 20:12:27 +0200

    Added splitVector function

--- global.h
+++ global.h
@@ -13,9 +13,24 @@ TEST_INPUT;
 #include <fstream>
 #include <iostream>
 #include <stdexcept>
+#include <functional>
 #include "std_is_missing_stuff.h"
 
 void getConstants();
+
+template<typename T, typename Lambda>
+std::vector<std::vector<T> > splitVector(std::vector<T> origin, Lambda& shouldSplit) {
+  std::vector<std::vector<T> > vec2d {std::vector<T>()};
+  long long vec2dIndex = 0;
+  for (T elem : origin) {
+    vec2d[vec2dIndex].push_back(elem);
+    if (shouldSplit(elem)) {
+      vec2dIndex++;
+      vec2d.push_back(std::vector<T>());
+    }
+  }
+  return vec2d;
+}
 
 template<typename T>
 void print(T thing) {
