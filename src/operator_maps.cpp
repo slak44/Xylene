9d3b3c46f201ed7c1ea530eaf5a38224b4dce3c2
Slak44 <sylviu44@gmail.com>
2015-11-17 17:33:11 +0200

    Added more operator maps

--- src/operator_maps.cpp
+++ src/operator_maps.cpp
@@ -3,6 +3,11 @@
 namespace lang {
   OperatorMap opsMap = {
     {Operator("+", 10), {
+      {"String String", boost::any(new Object::BinaryOp([](Object* l, Object* r) {
+        auto left = dynamic_cast<String*>(l);
+        auto right = dynamic_cast<String*>(r);
+        return new String(left->toString() + right->toString());
+      }))},
       {"Integer Integer", boost::any(new Object::BinaryOp([](Object* l, Object* r) {
         Integer* left = dynamic_cast<Integer*>(l);
         Integer* right = dynamic_cast<Integer*>(r);
@@ -11,6 +16,16 @@ namespace lang {
       {"Float Float", boost::any(new Object::BinaryOp([](Object* l, Object* r) {
         Float* left = dynamic_cast<Float*>(l);
         Float* right = dynamic_cast<Float*>(r);
+        return new Float(left->getNumber() + right->getNumber());
+      }))},
+      {"Integer Float", boost::any(new Object::BinaryOp([](Object* l, Object* r) {
+        Integer* left = dynamic_cast<Integer*>(l);
+        Float* right = dynamic_cast<Float*>(r);
+        return new Float(left->getNumber() + right->getNumber());
+      }))},
+      {"Float Integer", boost::any(new Object::BinaryOp([](Object* l, Object* r) {
+        Float* left = dynamic_cast<Float*>(l);
+        Integer* right = dynamic_cast<Integer*>(r);
         return new Float(left->getNumber() + right->getNumber());
       }))}
     }},
