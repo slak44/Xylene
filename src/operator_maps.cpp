b378685a10afef7f66c134a7d530a18eda2d9544
Slak44 <sylviu44@gmail.com>
2015-11-18 21:52:25 +0200

    Created macro for operator function map

--- src/operator_maps.cpp
+++ src/operator_maps.cpp
@@ -1,45 +1,29 @@
 #include "builtins.hpp"
 
+#define Q(x) #x
+#define QUOTE(x) Q(x)
+
+#define MAKE_BINARY_OP(type1, type2, returnData) \
+QUOTE(type1 type2), boost::any(new Object::BinaryOp([](Object* l, Object* r) {\
+  auto left = dynamic_cast<type1*>(l);\
+  auto right = dynamic_cast<type2*>(r);\
+  return returnData;\
+}))
+
 namespace lang {
   OperatorMap opsMap = {
     {Operator("+", 10), {
-      {"String String", boost::any(new Object::BinaryOp([](Object* l, Object* r) {
-        auto left = dynamic_cast<String*>(l);
-        auto right = dynamic_cast<String*>(r);
-        return new String(left->toString() + right->toString());
-      }))},
-      {"Integer Integer", boost::any(new Object::BinaryOp([](Object* l, Object* r) {
-        Integer* left = dynamic_cast<Integer*>(l);
-        Integer* right = dynamic_cast<Integer*>(r);
-        return new Integer(left->getNumber() + right->getNumber());
-      }))},
-      {"Float Float", boost::any(new Object::BinaryOp([](Object* l, Object* r) {
-        Float* left = dynamic_cast<Float*>(l);
-        Float* right = dynamic_cast<Float*>(r);
-        return new Float(left->getNumber() + right->getNumber());
-      }))},
-      {"Integer Float", boost::any(new Object::BinaryOp([](Object* l, Object* r) {
-        Integer* left = dynamic_cast<Integer*>(l);
-        Float* right = dynamic_cast<Float*>(r);
-        return new Float(left->getNumber() + right->getNumber());
-      }))},
-      {"Float Integer", boost::any(new Object::BinaryOp([](Object* l, Object* r) {
-        Float* left = dynamic_cast<Float*>(l);
-        Integer* right = dynamic_cast<Integer*>(r);
-        return new Float(left->getNumber() + right->getNumber());
-      }))}
+      // String concatenation
+      {MAKE_BINARY_OP(String, String, new String(left->toString() + right->toString()) )},
+      // Operations with numbers
+      {MAKE_BINARY_OP(Integer, Integer, new Integer(left->getNumber() + right->getNumber()) )},
+      {MAKE_BINARY_OP(Float, Float, new Float(left->getNumber() + right->getNumber()) )},
+      {MAKE_BINARY_OP(Integer, Float, new Float(left->getNumber() + right->getNumber()) )},
+      {MAKE_BINARY_OP(Float, Integer, new Float(left->getNumber() + right->getNumber()) )}
     }},
     {Operator("-", 10), {
-      {"Integer Integer", boost::any(new Object::BinaryOp([](Object* l, Object* r) {
-        Integer* left = dynamic_cast<Integer*>(l);
-        Integer* right = dynamic_cast<Integer*>(r);
-        return new Integer(left->getNumber() - right->getNumber());
-      }))},
-      {"Float Float", boost::any(new Object::BinaryOp([](Object* l, Object* r) {
-        Float* left = dynamic_cast<Float*>(l);
-        Float* right = dynamic_cast<Float*>(r);
-        return new Float(left->getNumber() - right->getNumber());
-      }))}
+      {MAKE_BINARY_OP(Integer, Integer, new Integer(left->getNumber() - right->getNumber()) )},
+      {MAKE_BINARY_OP(Float, Float, new Float(left->getNumber() - right->getNumber()) )},
     }},
   };
 } /* namespace lang */
