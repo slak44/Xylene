5dc98f01bf680631d6745b683d4a733ea9f30001
Slak44 <sylviu44@gmail.com>
2016-07-02 16:48:54 +0300

    Added class for managing scope

---
+++ src/scope.hpp
@@ -0,0 +1,49 @@
+#ifndef SCOPE_HPP
+#define SCOPE_HPP
+
+#include <unordered_map>
+#include <string>
+
+#include "object.hpp"
+#include "error.hpp"
+
+class NotFoundError: public InternalError {
+public:
+  NotFoundError(ErrorData data):
+    InternalError("NotFoundError", "Cannot find required identifier in this scope", data) {}
+};
+
+class Scope {
+private:
+  PtrUtil<Scope>::WeakLink parent;
+  std::unordered_map<std::string, Reference> map {};
+public:
+  typedef PtrUtil<Scope>::Link Link;
+  typedef PtrUtil<Scope>::WeakLink WeakLink;
+  
+  Reference get(std::string ident) const {
+    WeakLink lastParent = parent;
+    while (!lastParent.expired()) {
+      try {
+        return map.at(ident);
+      } catch (std::out_of_range& oor) {
+        lastParent = parent.lock()->getParent();
+      }
+    }
+    throw NotFoundError({METADATA_PAIRS, {"identifier", ident}});
+  }
+  
+  void set(std::string ident, Object::Link obj) {
+    map[ident] = Reference(obj);
+  }
+  
+  WeakLink getParent() const {
+    return parent;
+  }
+  
+  void setParent(WeakLink newParent) {
+    parent = newParent;
+  }
+};
+
+#endif
