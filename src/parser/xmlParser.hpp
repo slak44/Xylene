ca6dbf0a147df3d890a4289d053dbb577e1a8c73
Slak44 <sylviu44@gmail.com>
2016-08-08 20:17:03 +0300

    Documentation and reordering

--- src/parser/xmlParser.hpp
+++ src/parser/xmlParser.hpp
@@ -13,7 +13,59 @@ public:
     InternalError("XMLParseError", msg, data) {}
 };
 
+/*
+  XML files should contain a single root tag. The root tag must represent a block node.
+  List of tags:
+  - block: no attributes, maps to a BlockNode
+  - return: no attributes, has 0 or 1 expr tags as his children, maps to a ReturnNode
+  - expr: maps to an ExpressionNode
+    - attribute type: is a string name for a TokenType
+    - attribute value: equivalent to Token's data field (for Operators it is the string name)
+    - children: can be other expr nodes, as many as the operator's arity
+  - decl: maps to a DeclarationNode
+    - attribute ident: name of declared thing
+    - attribute dynamic: if equal to "true", declaration has dynamic type
+    - attribute types: space-separated list of acceptable types (eg "Integer Float"). Is a no-op if dynamic is true
+    - children: only one expr node
+  - branch: maps to a BranchNode
+    - children: an expr node that is the condition, a success block, and either a failiure block or another branch node
+  - loop: maps to a LoopNode
+    - children: one or none of each node: loop_init, loop_condition, loop_update. A block is mandatory
+  - loop_init: only inside a loop tag
+    - children: one expr node
+  - loop_condition: only inside a loop tag
+    - children: one expr node
+  - loop_update: only inside a loop tag
+    - children: one expr node
+*/
+
 class XMLParser: public BaseParser {
+public:
+  XMLParser() {}
+  
+  void parse(char* str) {
+    rapidxml::xml_document<char> doc;
+    try {
+      doc.parse<0>(str);
+    } catch (rapidxml::parse_error& pe) {
+      throw XMLParseError("XML parsing failed", {
+        METADATA_PAIRS,
+        {"xml error", pe.what()},
+        {"position", std::to_string(*pe.where<int>())}
+      });
+    }
+    auto rootNode = Node<BlockNode>::dynPtrCast(parseXMLNode(doc.first_node()));
+    if (rootNode == 0) throw XMLParseError("Root node isn't a block node", {
+      METADATA_PAIRS,
+      {"tag name", doc.first_node()->name()}
+    });
+    tree.setRoot(*rootNode);
+  }
+  
+  void parse(rapidxml::file<> xmlFile) {
+    parse(xmlFile.data());
+  }
+  
 private:
   void parseChildren(rapidxml::xml_node<>* node, ASTNode::Link target) {
     for (auto child = node->first_node(); child; child = child->next_sibling()) {
@@ -99,27 +151,6 @@ private:
       return parseXMLNode(node->first_node());
     }
     throw XMLParseError("Unknown type of node", {METADATA_PAIRS, {"node name", name}});
-  }
-public:
-  XMLParser() {}
-  
-  void parse(char* str) {
-    rapidxml::xml_document<char> doc;
-    try {
-      doc.parse<0>(str);
-    } catch (rapidxml::parse_error& pe) {
-      throw XMLParseError("XML parsing failed", {
-        METADATA_PAIRS,
-        {"xml error", pe.what()},
-        {"position", std::to_string(*pe.where<int>())}
-      });
-    }
-    auto root = Node<BlockNode>::dynPtrCast(parseXMLNode(doc.first_node()));
-    tree.setRoot(*root);
-  }
-  
-  void parse(rapidxml::file<> xmlFile) {
-    parse(xmlFile.data());
   }
 };
 
