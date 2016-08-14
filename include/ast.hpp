bbe8d81a42176324351929a09cf1735644946c69
Slak44 <sylviu44@gmail.com>
2016-08-14 19:49:16 +0300

    Reorganized AST nodes

---
+++ include/ast.hpp
@@ -0,0 +1,205 @@
+#ifndef AST_HPP
+#define AST_HPP
+
+#include <string>
+#include <vector>
+#include <memory>
+#include <typeinfo>
+
+#include "utils/util.hpp"
+#include "utils/error.hpp"
+#include "utils/typeInfo.hpp"
+#include "token.hpp"
+
+class ASTVisitor;
+using ASTVisitorLink = PtrUtil<ASTVisitor>::Link;
+
+class ASTNode: public std::enable_shared_from_this<ASTNode> {
+public:
+  using Link = std::shared_ptr<ASTNode>;
+  using WeakLink = std::weak_ptr<ASTNode>;
+  using Children = std::vector<Link>;
+protected:
+  WeakLink parent = WeakLink();
+  Children children {};
+  uint64 lineNumber = 0;
+  
+  static inline void printIndent(uint level) {
+    for (uint i = 0; i < level; i++) print("  ");
+  }
+public:
+  ASTNode(uint64 lineNumber = 0);
+  virtual ~ASTNode();
+  
+  virtual void addChild(Link child);
+  
+  Children getChildren() const;
+  Link at(int64 pos) const;
+  
+  void setParent(WeakLink newParent);
+  WeakLink getParent() const;
+  
+  void setLineNumber(uint64 newLineNumber);
+  uint64 getLineNumber() const;
+  
+  virtual void printTree(uint level) const;
+  
+  virtual bool operator==(const ASTNode& rhs) const;
+  virtual bool operator!=(const ASTNode& rhs) const;
+  
+  virtual void visit(ASTVisitorLink visitor) = 0;
+};
+
+class BlockNode: public ASTNode {
+public:
+  void printTree(uint level) const;
+  
+  void visit(ASTVisitorLink visitor);
+};
+
+class ExpressionNode: public ASTNode {
+private:
+  Token tok = Token(UNPROCESSED, 0);
+public:
+  ExpressionNode();
+  ExpressionNode(Token token);
+  
+  std::shared_ptr<ExpressionNode> at(int64 pos) const;
+  Token getToken() const;
+  
+  void printTree(uint level) const;
+  
+  bool operator==(const ASTNode& rhs) const;
+  bool operator!=(const ASTNode& rhs) const;
+  
+  void visit(ASTVisitorLink visitor);
+};
+
+template<typename T, typename std::enable_if<std::is_base_of<ASTNode, T>::value>::type* = nullptr>
+struct Node: public PtrUtil<T> {
+  typedef typename PtrUtil<T>::Link Link;
+  typedef typename PtrUtil<T>::WeakLink WeakLink;
+  
+  static inline bool isSameType(ASTNode::Link node) {
+    return typeid(T) == typeid(*node);
+  }
+  
+  static inline Link dynPtrCast(ASTNode::Link node) {
+    return std::dynamic_pointer_cast<T>(node);
+  }
+};
+
+class AST {
+private:
+  BlockNode root = BlockNode();
+public:
+  void print() const;
+  void setRoot(BlockNode rootNode);
+  BlockNode getRoot() const;
+  Node<BlockNode>::Link getRootAsLink() const;
+  
+  bool operator==(const AST& rhs) const;
+  bool operator!=(const AST& rhs) const;
+};
+
+#define GET_SIG(linkType, nameOf) Node<linkType>::Link get##nameOf() const;
+#define SET_SIG(linkType, nameOf) void set##nameOf(std::shared_ptr<linkType> newNode);
+#define GET_SET_SIGS(linkType, nameOf) \
+GET_SIG(linkType, nameOf) \
+SET_SIG(linkType, nameOf)
+
+;
+
+class NoMoreChildrenNode: public ASTNode {
+public:
+  NoMoreChildrenNode(int childrenCount);
+  
+  void addChild(Link child);
+  
+  bool inline notNull(uint childIndex) const {
+    return children[childIndex] != nullptr;
+  }
+};
+
+class DeclarationNode: public NoMoreChildrenNode {
+private:
+  std::string identifier;
+  DefiniteTypeInfo info;
+public:
+  DeclarationNode(std::string identifier, TypeList typeList);
+    
+  GET_SET_SIGS(ExpressionNode, Init)
+  
+  std::string getIdentifier() const;
+  DefiniteTypeInfo getTypeInfo() const;
+  
+  bool isDynamic() const;
+  bool hasInit() const;
+  
+  void printTree(uint level) const;
+  
+  bool operator==(const ASTNode& rhs) const;
+  bool operator!=(const ASTNode& rhs) const;
+  
+  void visit(ASTVisitorLink visitor);
+};
+
+class BranchNode: public NoMoreChildrenNode {
+public:
+  BranchNode();
+  
+  GET_SET_SIGS(ExpressionNode, Condition)
+  GET_SET_SIGS(BlockNode, SuccessBlock)
+  GET_SIG(ASTNode, FailiureBlock)
+  SET_SIG(BlockNode, FailiureBlock)
+  SET_SIG(BranchNode, FailiureBlock)
+  
+  void printTree(uint level) const;
+  
+  void visit(ASTVisitorLink visitor);
+};
+
+class LoopNode: public NoMoreChildrenNode {
+public:
+  LoopNode();
+  
+  GET_SET_SIGS(DeclarationNode, Init)
+  GET_SET_SIGS(ExpressionNode, Condition)
+  GET_SET_SIGS(ExpressionNode, Update)
+  GET_SET_SIGS(BlockNode, Code)
+  
+  void printTree(uint level) const;
+  
+  void visit(ASTVisitorLink visitor);
+};
+
+class ReturnNode: public NoMoreChildrenNode {
+public:
+  ReturnNode();
+  
+  GET_SET_SIGS(ExpressionNode, Value)
+  
+  void printTree(uint level) const;
+  
+  void visit(ASTVisitorLink visitor);
+};
+
+#undef GET_SIG
+#undef SET_SIG
+#undef GET_SET_SIGS
+
+#define PURE_VIRTUAL_VISIT(nodeName) virtual void visit##nodeName(nodeName##Node* node) = 0;
+
+class ASTVisitor {
+public:
+  PURE_VIRTUAL_VISIT(Block)
+  PURE_VIRTUAL_VISIT(Expression)
+  PURE_VIRTUAL_VISIT(Declaration)
+  PURE_VIRTUAL_VISIT(Branch)
+  PURE_VIRTUAL_VISIT(Loop)
+  PURE_VIRTUAL_VISIT(Return)
+};
+
+#undef PURE_VIRTUAL_VISIT
+
+#endif
---
+++ src/ast.cpp
@@ -0,0 +1,257 @@
+#include "ast.hpp"
+
+ASTNode::ASTNode(uint64 lineNumber): lineNumber(lineNumber) {}
+ASTNode::~ASTNode() {};
+
+void ASTNode::addChild(Link child) {
+  child->setParent(shared_from_this());
+  children.push_back(child);
+}
+
+ASTNode::Children ASTNode::getChildren() const {
+  return children;
+}
+
+ASTNode::Link ASTNode::at(int64 pos) const {
+  if (pos < 0) {
+    pos = children.size() + pos; // Negative indices count from the end of the vector
+  }
+  if (static_cast<std::size_t>(abs(pos)) > children.size() || pos < 0) {
+    throw InternalError("Index out of array bounds", {METADATA_PAIRS, {"index", std::to_string(pos)}});
+  }
+  return children[pos];
+}
+
+void ASTNode::setParent(WeakLink newParent) {parent = newParent;}
+ASTNode::WeakLink ASTNode::getParent() const {return parent;}
+
+void ASTNode::setLineNumber(uint64 newLineNumber) {
+  lineNumber = newLineNumber;
+}
+uint64 ASTNode::getLineNumber() const {
+  return lineNumber;
+}
+
+bool ASTNode::operator==(const ASTNode& rhs) const {
+  if (typeid(*this) != typeid(rhs)) return false;
+  if (children.size() != rhs.getChildren().size()) return false;
+  for (uint64 i = 0; i < children.size(); i++) {
+    if (*(this->at(i)) != *(rhs.at(i))) return false;
+  }
+  return true;
+}
+
+bool ASTNode::operator!=(const ASTNode& rhs) const {
+  return !operator==(rhs);
+}
+
+NoMoreChildrenNode::NoMoreChildrenNode(int childrenCount) {
+  children.resize(childrenCount, nullptr);
+}
+
+void NoMoreChildrenNode::addChild(Link child) {
+  UNUSED(child);
+  throw InternalError("Cannot add children to NoMoreChildrenNode", {METADATA_PAIRS});
+}
+
+ExpressionNode::ExpressionNode() {}
+ExpressionNode::ExpressionNode(Token token): tok(token) {
+  switch (tok.type) {
+    case IDENTIFIER:
+    case OPERATOR:
+    case L_INTEGER:
+    case L_FLOAT:
+    case L_STRING:
+    case L_BOOLEAN:
+      break;
+    default: throw InternalError("Trying to add unsupported token to ExpressionNode", {METADATA_PAIRS, {"token", token.toString()}});
+  }
+}
+
+std::shared_ptr<ExpressionNode> ExpressionNode::at(int64 pos) const {
+  return std::dynamic_pointer_cast<ExpressionNode>(ASTNode::at(pos));
+}
+
+Token ExpressionNode::getToken() const {
+  return tok;
+}
+
+bool ExpressionNode::operator==(const ASTNode& rhs) const {
+  if (!ASTNode::operator==(rhs)) return false;
+  if (this->tok != dynamic_cast<const ExpressionNode&>(rhs).tok) return false;
+  return true;
+}
+bool ExpressionNode::operator!=(const ASTNode& rhs) const {
+  return !operator==(rhs);
+}
+
+DeclarationNode::DeclarationNode(std::string identifier, TypeList typeList):
+  NoMoreChildrenNode(1), identifier(identifier), info(typeList) {}
+  
+std::string DeclarationNode::getIdentifier() const {
+  return identifier;
+}
+
+DefiniteTypeInfo DeclarationNode::getTypeInfo() const {
+  return info;
+}
+
+bool DeclarationNode::isDynamic() const {
+  return info.isDynamic();
+}
+
+bool DeclarationNode::hasInit() const {
+  return children[0] != nullptr;
+}
+
+bool DeclarationNode::operator==(const ASTNode& rhs) const {
+  if (!ASTNode::operator==(rhs)) return false;
+  auto decl = dynamic_cast<const DeclarationNode&>(rhs);
+  if (this->identifier != decl.identifier) return false;
+  if (this->info != decl.info) return false;
+  return true;
+}
+bool DeclarationNode::operator!=(const ASTNode& rhs) const {
+  return !operator==(rhs);
+}
+
+BranchNode::BranchNode(): NoMoreChildrenNode(3) {}
+
+LoopNode::LoopNode(): NoMoreChildrenNode(4) {}
+
+ReturnNode::ReturnNode(): NoMoreChildrenNode(1) {}
+
+// Macro for easy printing of NoMoreChildrenNode children
+#define PRETTY_PRINT_FOR(childIndex, nameOf) \
+{\
+  printIndent(level + 1);\
+  println(#nameOf":");\
+  children[childIndex]->printTree(level + 1);\
+}
+
+void ASTNode::printTree(uint level) const {
+  printIndent(level);
+  println("Base ASTNode");
+  for (auto& child : children) child->printTree(level + 1);
+}
+
+void BlockNode::printTree(uint level) const {
+  printIndent(level);
+  println("Block Node");
+  for (auto& child : children) child->printTree(level + 1);
+}
+
+void ExpressionNode::printTree(uint level) const {
+  printIndent(level);
+  println("Expression Node:", tok);
+  for (auto& child : children) child->printTree(level + 1);
+}
+
+void DeclarationNode::printTree(uint level) const {
+  printIndent(level);
+  println("Declaration Node: " + identifier + " (" + info.toString() + ")");
+  if (children.size() > 0) children[0]->printTree(level + 1);
+}
+
+void BranchNode::printTree(uint level) const {
+  printIndent(level);
+  println("Branch Node:");
+  PRETTY_PRINT_FOR(0, Condition)
+  PRETTY_PRINT_FOR(1, Success)
+  if (notNull(2)) PRETTY_PRINT_FOR(2, Failiure)
+}
+
+void LoopNode::printTree(uint level) const {
+  printIndent(level);
+  println("Loop Node");
+  if (notNull(0)) PRETTY_PRINT_FOR(0, Init)
+  if (notNull(1)) PRETTY_PRINT_FOR(1, Condition)
+  if (notNull(2)) PRETTY_PRINT_FOR(2, Update)
+  if (notNull(3)) PRETTY_PRINT_FOR(3, Code)
+}
+
+void ReturnNode::printTree(uint level) const {
+  printIndent(level);
+  println("Return Node");
+  if (notNull(0)) PRETTY_PRINT_FOR(0, Value)
+}
+
+#undef PRETTY_PRINT_FOR
+
+// Macros for easy implementation of getters and setters for NoMoreChildrenNode subclasses
+// Omologues exist in header to provide signatures for these implementations
+// srcNode: name of subclass
+// childIndex: index for the child (because number of children is fixed on NoMoreChildrenNodes)
+// nameOf: name for getter/setter (get##nameOf and set##nameOf)
+// linkType: type of child
+#define GET_FOR(srcNode, childIndex, nameOf, linkType) \
+Node<linkType>::Link srcNode::get##nameOf() const {\
+  return Node<linkType>::dynPtrCast(children[childIndex]);\
+}
+#define SET_FOR(srcNode, childIndex, nameOf, linkType) \
+void srcNode::set##nameOf(std::shared_ptr<linkType> newNode) {children[childIndex] = newNode;}
+
+#define GET_SET_FOR(srcNode, childIndex, nameOf, linkType) \
+GET_FOR(srcNode, childIndex, nameOf, linkType); \
+SET_FOR(srcNode, childIndex, nameOf, linkType);
+
+GET_SET_FOR(DeclarationNode, 0, Init, ExpressionNode);
+
+GET_SET_FOR(BranchNode, 0, Condition, ExpressionNode);
+GET_SET_FOR(BranchNode, 1, SuccessBlock, BlockNode);
+GET_FOR(BranchNode, 2, FailiureBlock, ASTNode);
+SET_FOR(BranchNode, 2, FailiureBlock, BlockNode);
+SET_FOR(BranchNode, 2, FailiureBlock, BranchNode);
+
+GET_SET_FOR(LoopNode, 0, Init, DeclarationNode);
+GET_SET_FOR(LoopNode, 1, Condition, ExpressionNode);
+GET_SET_FOR(LoopNode, 2, Update, ExpressionNode);
+GET_SET_FOR(LoopNode, 3, Code, BlockNode);
+
+GET_SET_FOR(ReturnNode, 0, Value, ExpressionNode);
+
+#undef GET_FOR
+#undef SET_FOR
+#undef GET_SET_FOR
+
+// Macro to help implement the 'visit' functions in each node
+// nodeName: name of node class, without 'Node' at the end, eg Loop, not LoopNode
+#define VISITOR_VISIT_IMPL_FOR(nodeName) \
+void nodeName##Node::visit(ASTVisitorLink visitor) {\
+  visitor->visit##nodeName(this);\
+}
+
+VISITOR_VISIT_IMPL_FOR(Block);
+VISITOR_VISIT_IMPL_FOR(Expression);
+VISITOR_VISIT_IMPL_FOR(Declaration);
+VISITOR_VISIT_IMPL_FOR(Branch);
+VISITOR_VISIT_IMPL_FOR(Loop);
+VISITOR_VISIT_IMPL_FOR(Return);
+
+#undef VISITOR_VISIT_IMPL_FOR
+
+// AST class
+
+void AST::print() const {
+  root.printTree(0);
+}
+
+void AST::setRoot(BlockNode rootNode) {
+  root = rootNode;
+}
+
+BlockNode AST::getRoot() const {
+  return root;
+}
+
+Node<BlockNode>::Link AST::getRootAsLink() const {
+  return Node<BlockNode>::make(root);
+}
+
+bool AST::operator==(const AST& rhs) const {
+  if (this->root != rhs.root) return false;
+  return true;
+}
+bool AST::operator!=(const AST& rhs) const {
+  return !operator==(rhs);
+}
