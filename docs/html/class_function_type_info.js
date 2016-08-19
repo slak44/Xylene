84659a1b224f696a90cd30b594ba7438416db53f
Slak44 <sylviu44@gmail.com>
2016-08-19 14:08:06 +0300

    Add Doxyfile, fix docs path

--- docs/html/class_function_type_info.js
+++
@@ -1,5 +0,0 @@
-var class_function_type_info =
-[
-    [ "FunctionTypeInfo", "class_function_type_info.html#a49dac1da315fb179a10f9ff16f32cc6b", null ],
-    [ "toString", "class_function_type_info.html#a5980b1f223b6e42414cf78c69010ef2f", null ]
-];
--- docs/html/examples.js
+++
@@ -1,6 +0,0 @@
-var examples =
-[
-    [ "-", "--example.html", null ],
-    [ "Boolean", "_boolean-example.html", null ],
-    [ "if", "if-example.html", null ]
-];
