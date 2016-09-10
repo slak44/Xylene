846dd683b5dee3f0a78d6c6e6492eb9ca0683ea4
Slak44 <sylviu44@gmail.com>
2016-09-10 09:14:02 +0300

    Fixed missing libs

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include <llvm/IR/Module.h>
 #include <llvm/ExecutionEngine/GenericValue.h>
 #include <llvm/ExecutionEngine/ExecutionEngine.h>
-#include <llvm/ExecutionEngine/Interpreter.h>
+#include <llvm/ExecutionEngine/MCJIT.h>
 #include <llvm/Support/TargetSelect.h>
 #include <fstream>
 #include <sstream>
