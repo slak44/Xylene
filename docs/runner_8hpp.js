610c9752dc02b37e31fb574567483c88de573d8d
Stefan Silviu <sylviu44@gmail.com>
2017-01-08 21:27:30 +0200

    Update docs

--- docs/runner_8hpp.js
+++ docs/runner_8hpp.js
@@ -1,5 +1,6 @@
 var runner_8hpp =
 [
     [ "Runner", "class_runner.html", "class_runner" ],
-    [ "ProgramResult", "runner_8hpp.html#a9ff1de259e8e3ff2e53ba9d5dbde4f40", null ]
+    [ "ProgramResult", "runner_8hpp.html#a9ff1de259e8e3ff2e53ba9d5dbde4f40", null ],
+    [ "nameToFunPtr", "runner_8hpp.html#a5e05d41aebefa74430b2973af66f743a", null ]
 ];
