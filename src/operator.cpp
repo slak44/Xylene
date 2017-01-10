634efef32577891b1c9ed087a699f70990d1ebe2
Stefan Silviu <sylviu44@gmail.com>
2017-01-10 18:05:26 +0200

    Add missing value

--- src/operator.cpp
+++ src/operator.cpp
@@ -110,7 +110,7 @@ const std::vector<Operator> Operator::list {
   // constructs. The expressions using those are created in the parser
   Operator("[]", 13, "Subscript", ASSOCIATE_FROM_LEFT, BINARY, CIRCUMFIX, {true, false}),
   // Call op will contain name of function + argument tree with commas
-  Operator("()", 13, "Call", ASSOCIATE_FROM_LEFT, BINARY, POSTFIX, {true}),
+  Operator("()", 13, "Call", ASSOCIATE_FROM_LEFT, BINARY, POSTFIX, {true, false}),
   Operator("?:", 1, "Conditional", ASSOCIATE_FROM_LEFT, TERNARY, CIRCUMFIX),
   Operator(" ", 0, "No-op", ASSOCIATE_FROM_LEFT, NULLARY)
 };
