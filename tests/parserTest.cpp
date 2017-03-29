08e56717e4433f744ff76b95378b5d74c2003af2
Stefan Silviu <sylviu44@gmail.com>
2017-03-29 21:05:43 +0300

    Rename function

--- tests/parserTest.cpp
+++ tests/parserTest.cpp
@@ -16,7 +16,7 @@ protected:
 
 class ParserCompareTest: public ParserTest {
 protected:
-  inline void test(std::string code, std::string xmlCodePath) {
+  inline void compare(std::string code, std::string xmlCodePath) {
     auto pxTr = TokenParser::parse(Lexer::tokenize(code, xmlCodePath)->getTokens());
     auto xpxTr = XMLParser::parse(xmlFile(xmlCodePath));
     EXPECT_EQ(pxTr, xpxTr);
@@ -24,48 +24,48 @@ protected:
 };
 
 TEST_F(ParserCompareTest, Expressions) {
-  test("+ -- a ++ --;", "data/parser/unary.xml");
-  test("a + 1;", "data/parser/expr/start_with_ident.xml");
-  test("-(a + b)++;", "data/parser/expr/parens_unaries.xml");
+  compare("+ -- a ++ --;", "data/parser/unary.xml");
+  compare("a + 1;", "data/parser/expr/start_with_ident.xml");
+  compare("-(a + b)++;", "data/parser/expr/parens_unaries.xml");
 }
 
 TEST_F(ParserCompareTest, FunctionCalls) {
-  test("a();", "data/parser/expr/calls/no_args.xml");
-  test("a(42);", "data/parser/expr/calls/one_terminal_arg.xml");
-  test("a(1+1);", "data/parser/expr/calls/one_expr_arg.xml");
-  test("a(42, 6714);", "data/parser/expr/calls/2_args.xml");
-  test("a(1+1, 3 * (5 + 5));", "data/parser/expr/calls/expression_args.xml");
-  test("-a++();", "data/parser/expr/calls/after_postfix.xml");
+  compare("a();", "data/parser/expr/calls/no_args.xml");
+  compare("a(42);", "data/parser/expr/calls/one_terminal_arg.xml");
+  compare("a(1+1);", "data/parser/expr/calls/one_expr_arg.xml");
+  compare("a(42, 6714);", "data/parser/expr/calls/2_args.xml");
+  compare("a(1+1, 3 * (5 + 5));", "data/parser/expr/calls/expression_args.xml");
+  compare("-a++();", "data/parser/expr/calls/after_postfix.xml");
 }
 
 TEST_F(ParserCompareTest, Functions) {
-  test(R"code(
+  compare(R"code(
     function add [Integer, Float a, Integer, Float b] => Integer, Float do
     end
   )code", "data/parser/functions/complete.xml");
-  test(R"code(
+  compare(R"code(
     function add [Integer, Float a, Integer, Float b] do
     end
   )code", "data/parser/functions/void_ret.xml");
-  test(R"code(
+  compare(R"code(
     function add => Integer do
     end
   )code", "data/parser/functions/no_args.xml");
-  test(R"code(
+  compare(R"code(
     function add do
     end
   )code", "data/parser/functions/plain.xml");
-  test(R"code(
+  compare(R"code(
     function [Integer e] => Integer do
     end
   )code", "data/parser/functions/anon.xml");
-  test(R"code(
+  compare(R"code(
     foreign function putchar [Integer char] => Integer;
   )code", "data/parser/functions/foreign.xml");
 }
 
 TEST_F(ParserCompareTest, IfStatement) {
-  test(R"code(
+  compare(R"code(
     if true == false do
       1 + 2;
     else do
@@ -75,7 +75,7 @@ TEST_F(ParserCompareTest, IfStatement) {
 }
 
 TEST_F(ParserCompareTest, Declarations) {
-  test(R"code(
+  compare(R"code(
     define a = 1;
     Integer i = 2;
     Float, Integer nr = 3.3;
@@ -83,22 +83,22 @@ TEST_F(ParserCompareTest, Declarations) {
 }
 
 TEST_F(ParserCompareTest, ForLoop) {
-  test(R"code(
+  compare(R"code(
     for define x = 1; x < 3; do
       1+1;
     end
   )code", "data/parser/for_loops/no_updates.xml");
-  test(R"code(
+  compare(R"code(
     for define x = 1; x < 3; ++x do
       1+1;
     end
   )code", "data/parser/for_loops/for_loop.xml");
-  test(R"code(
+  compare(R"code(
     for define x = 1; x < 6; ++x, ++x do
       1+1;
     end
   )code", "data/parser/for_loops/2_updates.xml");
-  test(R"code(
+  compare(R"code(
     for define x = 1; x < 6; ++x, ++x, --x do
       1+1;
     end
@@ -106,13 +106,13 @@ TEST_F(ParserCompareTest, ForLoop) {
 }
 
 TEST_F(ParserCompareTest, ReturnStatement) {
-  test(R"code(
+  compare(R"code(
     return 1 + 1;
   )code", "data/parser/simple_return.xml");
 }
 
 TEST_F(ParserCompareTest, Type) {
-  test(R"code(
+  compare(R"code(
     type CoolType inherits from CoolParentType do
       Integer i = 0;
       public constructor [Integer int] do
